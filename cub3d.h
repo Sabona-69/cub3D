c313387ce2c0d7f15e9357029d39e125360e7c49
Hamza El Omrani <hel-omra@e3r1p1.1337.ma>
2024-09-09 05:17:46 +0100

    /latest update/

--- cub3d.h
+++ cub3d.h
@@ -7,27 +7,20 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <limits.h>
+# include <math.h>
+# include <mlx.h>
 # include "libft/libft.h"
 # define BUFFER_SIZE 1024
 # define TRUE 1
 # define FALSE 0
 
-// utils
-// int		till_char(char *s, char c);
-char	**strjoin2d (char **str, char *s);
-char	*ft_strtrim_end(char const *s1, char const *set);
-char	*get_next_line(int fd);
-void	free_it(char **s, int i);
-int		skip_char(char *s, char c);
-int		ft_strlen2d(char **s);
-int		is_empty(char *s);
-int		my_atoi(char *str);
 
 
-typedef struct s_parse
+typedef struct s_pos
 {
-
-}	t_parse;
+	int		x;
+	int		y;
+}	t_pos;
 
 
 typedef struct s_data
@@ -37,11 +30,27 @@ typedef struct s_data
 	char	*SO;
 	char	*WE;
 	char	*EA;
-	char	Player_postition;
+	char	player_facing;
+	t_pos	player_postion;
 	int		F[3];
 	int		C[3];
 	int		fd;
 	char	*line;
 }   t_data;
+
+// utils
+// int		till_char(char *s, char c);
+char	**strjoin2d(char **str, char *s);
+char	*ft_strtrim_end(char const *s1, char const *set);
+char	*get_next_line(int fd);
+void	free_it(char **s, int i);
+int		skip_char(char *s, char c);
+int		ft_strlen2d(char **s);
+int		is_empty(char *s);
+int		my_atoi(char *str);
+void	parse_it(char *s, t_data *cub);
+
+
+
 
 #endif
