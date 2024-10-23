d9e1fb24b4b747b2300536179f8e3259f8322a74
Hamza El Omrani <hel-omra@e3r3p9.1337.ma>
2024-10-23 12:36:51 +0100

    /latest update/

--- mandatory/parse/map.c
+++ mandatory/parse/map.c
@@ -16,58 +16,6 @@ void	exiting(t_data *cub, int status)
 	exit(1);
 }
 
-char	*get_texture_line(char *s, t_data *cub)
-{
-	char	**split;
-	char	*new;
-	int		check;
-
-	split = ft_split(s, " ");
-	if (ft_strlen2d(split) != 2)
-		exiting(cub, 1);
-	new = ft_strdup(split[1]);
-	check = open(new, O_RDONLY);
-	if (check == -1)
-		exiting(cub, 1);
-	close(check);
-	free2d(split, ft_strlen2d(split));
-	return (new);
-}
-
-void	get_colors(int *tab, char *s, t_data *cub)
-{
-	char **split;
-	char *tmp;
-	int i;
-	int j;
-
-	i = -1;
-	j = 0;
-	if (!ft_isdigit(*s) || !ft_isdigit(s[ft_strlen(s) - 1]))
-		exiting(cub, 1);
-	while (s[++i])
-	{
-		if (s[i] == ',')
-			j++;
-	}
-	if (j != 2)
-		exiting(cub, 1);
-	split = ft_split(s, ",");
-	if (ft_strlen2d(split) != 3)
-		exiting(cub, 1);
-	i = -1;
-	while (++i < 3)
-	{
-		tmp = ft_strtrim(split[i], " ");
-		tab[i] = my_atoi(tmp);
-		free(tmp);
-	}
-	if (tab[0] == -1 || tab[1] == -1 || tab[2] == -1)
-		exiting(cub, 1);
-
-	free2d(split, ft_strlen2d(split));
-}
-
 void	store_map(t_data *cub)
 {
 	char *tmp;
@@ -115,42 +63,7 @@ void	store_map(t_data *cub)
 	free2d(map, ft_strlen2d(map));
 }
 
-void store_instructions(char *s, t_data *cub)
-{
-	char	*tmp;
-	int		i;
 
-	i = 0;
-	cub->fd = open(s, O_RDONLY);
-	if (cub->fd == -1)
-		exiting(cub, 1);
-	cub->line = get_next_line(cub->fd);
-	while (cub->line)
-	{
-		tmp = ft_strtrim(cub->line, " \n");
-		if (ft_strncmp("NO ", tmp, 3) == 0)
-			(1) && (cub->NO = get_texture_line(tmp, cub), i++);
-		else if (ft_strncmp("SO ", tmp, 3) == 0)
-			(1) && (cub->SO = get_texture_line(tmp, cub), i++);
-		else if (ft_strncmp("WE ", tmp, 3) == 0)
-			(1) && (cub->WE = get_texture_line(tmp, cub), i++);
-		else if (ft_strncmp("EA ", tmp, 3) == 0)
-			(1) && (cub->EA = get_texture_line(tmp, cub), i++);
-		else if (ft_strncmp("F ", tmp, 2) == 0)
-			(get_colors(cub->F, tmp + 2, cub), i++);
-		else if (ft_strncmp("C ", tmp, 2) == 0)
-			(get_colors(cub->C, tmp + 2, cub), i++);
-		else if (i != 6 && tmp[0])
-			(free(tmp), exiting(cub, 1));
-		free(tmp);
-		free(cub->line);
-		cub->line = get_next_line(cub->fd);
-		if (i == 6)
-			break ;
-	}
-	if (i != 6 || cub->C[0] == -1 || cub->F[0] == -1 || !cub->EA || !cub->NO || !cub->SO || !cub->WE)
-		exiting(cub, 1);
-}
 
 void	check_elements(t_data *cub)
 {
